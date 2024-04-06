85e211f958fd53a66610872e97a3c7e891c5cd47
gkiplangat <gideonkiplangat4@gmail.com>
2024-04-06 16:29:47 +0300

    feat:dummy blogs

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app=Flask(__name__)
+
+posts =[
+    {
+        'title':'Blog Post 1',
+        'author': 'Gideon Kiplangat',
+        'date_posted':'6 April 2024',
+        'content': 'The is our first post, welcome'
+    },
+      {
+        'title':'Blog Post 2',
+        'author': 'Philip Kiprotich',
+        'date_posted':'7 April 2024',
+        'content': 'The is our second post, welcome'
+    }
+]
 
 #Home Page
 @app.route('/')
 @app.route('/home/')
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('home.html', posts=posts)
 
 # About Us Page
 @app.route('/about/')
 def about():
-    return "<h1>About Us</h1>"
+    return render_template('about.html')
 
 if __name__=='__main__':
     app.run(debug=True)
